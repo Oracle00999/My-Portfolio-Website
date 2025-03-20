64e0fbc94b4bc9a01ad6a65e7eb5f6ad7abe23fe
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2025-03-20 19:49:21 +0100

    Update About.jsx

--- portfolio 2.0/src/Components/About.jsx
+++ portfolio 2.0/src/Components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
   ];
 
   return (
-    <section id="about" className="py-20 bg-gray-100">
+    <section id="about" className="py-26 bg-gray-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           {/* Left Column: Text Content */}
